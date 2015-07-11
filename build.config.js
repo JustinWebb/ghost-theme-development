f7b7cd42b071d439e51016f1ce101d90a39afa1d
Justin Webb <jw@justinwebb.io>
2015-07-11 00:43:25 -0700

    (feat) Externalize the build configuration

--- build.config.js
+++ build.config.js
@@ -2,20 +2,32 @@
 * @Author: Justin Webb
 * @Date:   2015-07-03 19:45:10
 * @Last Modified by:   justinwebb
-* @Last Modified time: 2015-07-03 21:00:07
+* @Last Modified time: 2015-07-11 00:19:09
 */
 
 'use strict';
 
 module.exports  = {
   ghost: {
-    themes: '../../Ghost/content/themes',
-    src: 'dist/**/*'
+    app: { 
+      config: __dirname + '/ghost-config.js'
+    }
   },
-  dist : 'dist',
-  css_dir : 'dist/assets/css',
+  dist: 'dist',
+  dev: {
+    root: 'content/themes/dev',
+    assets: {
+      js: 'content/themes/dev/assets/js',
+      css: 'content/themes/dev/assets/css',
+      fonts: 'content/themes/dev/assets/fonts' 
+    }
+  },
   src_files : {
-    hbs: ['src/**/*.hbs'],
-    sass: ['src/assets/css/**.scss']
+    everything: ['/**/*'],
+    fonts: ['fonts/*.{eot,svg,ttf,woff,otf}'],
+    hbs: ['templates/**/*.hbs'],
+    js: ['js/**/*.js'],
+    scss: ['css/**.scss'],
+    support: ['support/**/*']
   }
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
 var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')();
-plugins.minifyCSS = require('gulp-minify-css'); // does not autoload
-plugins.gulpif = require('gulp-if'); // does not autoload
 var lazypipe = require('lazypipe');
+var plugins = require('gulp-load-plugins')();
+var config = require('./build.config');
+var autoprefixBrowsers = ['last 2 version', 'safari 5', 'ie 9', 'opera 12.1'];
+
+
+// add plugins that do not autoload
+plugins.minifyCSS = require('gulp-minify-css');
+plugins.gulpif = require('gulp-if');
+
 
 var onError = function (err) {
-	plugins.util.log(plugins.util.colors.red("Error"), err.message);
+	plugins.util.log(plugins.util.colors.red('Error'), err.message);
         this.emit('end');
 };
 
-function endsWith(str, suffix) {
+var endsWith = function (str, suffix) {
 	return str.indexOf(suffix, str.length - suffix.length) !== -1;
 }
 
@@ -18,126 +24,139 @@ function endsWith(str, suffix) {
 // minified version
 //
 gulp.task('sass', function () {
-	gulp.src('css/*.scss')
+	gulp.src(config.src_files.scss)
 		.pipe(plugins.plumber(onError))
 		.pipe(plugins.sourcemaps.init())
 		.pipe(plugins.sass())
 		.pipe(plugins.colorguard())
-		.pipe(plugins.autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1'))
+		.pipe(plugins.autoprefixer(autoprefixBrowsers))
 		.pipe(plugins.sourcemaps.write('.'))
-		.pipe(gulp.dest('content/themes/dev/assets/css'))
+		.pipe(gulp.dest(config.dev.assets.css));
 });
 
 gulp.task('sass_minify', function () {
-	gulp.src('css/*.scss')
+	gulp.src(config.src_files.scss)
 		.pipe(plugins.sass())
 		.pipe(plugins.colorguard())
-		.pipe(plugins.autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1'))
+		.pipe(plugins.autoprefixer(autoprefixBrowsers))
 		.pipe(plugins.minifyCSS())
-		.pipe(gulp.dest('content/themes/dev/assets/css'))
+		.pipe(gulp.dest(config.dev.assets.css));
 });
 
 //
 // concat all javascript files to site.js
 //
 gulp.task('js', function () {
-	gulp.src('js/*.js')
+	gulp.src(config.src_files.js)
 		.pipe(plugins.plumber(onError))
 		.pipe(plugins.sourcemaps.init())
 		.pipe(plugins.concat('site.js'))
 		.pipe(plugins.sourcemaps.write('.'))
-		.pipe(gulp.dest('content/themes/dev/assets/js'))
+		.pipe(gulp.dest(config.dev.assets.js))
 });
 
 gulp.task('js_minify', function () {
-	gulp.src('js/*.js')
+	gulp.src(config.src_files.js)
 		.pipe(plugins.concat('site.js'))
-		.pipe(gulp.dest('content/themes/dev/assets/js'))
+		.pipe(gulp.dest(config.dev.assets.js))
 });
 
 
 //
 // copy handlebars theme files over
 //
-gulp.task('templates_livereload', function() {
+gulp.task('templates_livereload', function () {
 	var embed_live_reload = lazypipe()
 		.pipe(plugins.rename, function (path) {
-				path.extname = ".html";
+				path.extname = '.html';
 			}
 		)
 		.pipe(plugins.embedlr)
 		.pipe(plugins.rename, function (path) {
-				path.extname = ".hbs";
+				path.extname = '.hbs';
 			}
 		);
 
-	gulp.src('templates/**/*.hbs')
+	gulp.src(config.src_files.hbs)
 		.pipe(plugins.plumber(onError))
 		.pipe(plugins.gulpif(function (file) {
-				return endsWith(file.path, "default.hbs");
+				return endsWith(file.path, 'default.hbs');
 			}, embed_live_reload()
 		))
-		.pipe(gulp.dest('content/themes/dev/'))
+		.pipe(gulp.dest(config.dev.root))
 });
 
-gulp.task('templates', function() {
-	gulp.src('templates/**/*.hbs')
-		.pipe(gulp.dest('content/themes/dev/'))
+gulp.task('templates', function () {
+	gulp.src(config.src_files.hbs)
+		.pipe(gulp.dest(config.dev.root))
 });
 
 //
-// copy stuff over
+// copy support over
 //
-gulp.task('stuff', function() {
-	gulp.src('stuff/*')
-		.pipe(gulp.dest('content/themes/dev/'))
+gulp.task('support', function () {
+	gulp.src(config.src_files.support)
+		.pipe(gulp.dest(config.dev.root))
 });
 
 //
 // copy font files over
 //
-gulp.task('fonts', function() {
-	gulp.src('fonts/*.{eot,svg,ttf,woff,otf}')
-		.pipe(gulp.dest('content/themes/dev/assets/fonts'))
+gulp.task('fonts', function () {
+	gulp.src(config.src_files.fonts)
+		.pipe(gulp.dest(config.dev.assets.fonts))
 });
 
 
 //
 // just run a live reload server and watch files for changes
 //
-gulp.task('livereload', ['sass', 'js', 'templates_livereload', 'fonts', 'stuff'], function() {
-	reloader = plugins.livereload("0.0.0.0:35729");
+gulp.task('livereload',
+	['sass', 'js', 'templates_livereload', 'fonts', 'support'],
+	function () {
+		reloader = plugins.livereload('0.0.0.0:35729');
 
-	gulp.watch('css/**/*.scss', ['sass']);
-	gulp.watch('templates/**/*.hbs', ['templates_livereload']);
-	gulp.watch('fonts/*.{eot,svg,ttf,woff,otf}', ['fonts']);
-	gulp.watch('stuff/*', ['stuff']);
-	gulp.watch('js/*.js', ['js']);
+		gulp.watch(config.src_files.sass, ['sass']);
+		gulp.watch(config.src_files.hbs, ['templates_livereload']);
+		gulp.watch(config.src_files.fonts, ['fonts']);
+		gulp.watch(config.src_files.support, ['support']);
+		gulp.watch(config.src_files.js, ['js']);
 
-	gulp.watch('content/themes/dev/**/*.css').on('change', function(file) {
-		reloader.changed(file.path);
-	});
-	gulp.watch('content/themes/dev/**/*.hbs').on('change', function(file) {
-		reloader.changed(file.path);
-	});
+		gulp.watch(config.dev.assets.css).on('change', function(file) {
+			reloader.changed(file.path);
+		});
+		gulp.watch(config.dev.assets.hbs).on('change', function(file) {
+			reloader.changed(file.path);
+		});
 
-	var ghost = require('ghost');
-	process.env.NODE_ENV = 'development';
-	ghost({ config: __dirname + '/ghost-config.js' }).then(function (ghostServer) {
-		ghostServer.start();
-	});
-});
+		process.env.NODE_ENV = 'development';
+		require('ghost')(config.ghost.app).then(function (ghostServer) {
+			ghostServer.start();
+		});
+	}
+);
 
-gulp.task('dist', ['default'], function() {
-	gulp.src('content/themes/dev/**/*')
+gulp.task('dist', ['build'], function () {
+	gulp.src(config.src_files.everything)
 		.pipe(plugins.gulpif(function (file) {
-				return !endsWith(file.path, ".map");
+				return !endsWith(file.path, '.map');
 			}, plugins.zip('dev-theme.zip')
 		))
 		.pipe(gulp.dest('.'));
 });
 
 //
-// default task, compile everything
+// compile source files into deployable files
 //
-gulp.task('default', ['sass_minify', 'js_minify', 'templates', 'fonts', 'stuff']);
+gulp.task('build', [
+	'sass_minify',
+	'js_minify',
+	'templates', 
+	'fonts', 
+	'support'
+]);
+
+//
+// default task, compile everything and launch preview server
+// 
+gulp.task('default', ['build', 'livereload']);
